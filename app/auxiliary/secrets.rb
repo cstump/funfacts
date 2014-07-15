2f655ca497b115ee9aa12a3ceb07428a73e23081
Chris Stump <chris@mighty-nice.com>
2014-07-14 20:55:17 -0500

    should implement #respond_to?

--- app/auxiliary/secrets.rb
+++ app/auxiliary/secrets.rb
@@ -5,6 +5,10 @@ class Secrets < SimpleDelegator
     super Hashie::Mash.new(Rails.application.secrets)
   end
 
+  def self.respond_to?(symbol, include_all = false)
+    instance.respond_to? symbol, include_all
+  end
+
   def self.method_missing(name, *args, &block)
     instance.send name, *args
   end
