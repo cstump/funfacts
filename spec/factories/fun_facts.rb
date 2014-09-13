f9881f11ab52d2cc64dddb24f57f264134b4006a
Chris Stump <chris@mighty-nice.com>
2014-09-12 21:08:16 -0500

    factories for the m-to-m relationship

--- spec/factories/fun_facts.rb
+++ spec/factories/fun_facts.rb
@@ -3,5 +3,13 @@ FactoryGirl.define do
     sequence(:position){|n| n }
     name Faker::Lorem.word
     description Faker::Lorem.sentence
+
+    factory :fact_with_datasets do
+      ignore { datasets_count 3 }
+
+      after :create do |fact, evaluator|
+        create_list :chicago_dataset, evaluator.datasets_count, fun_fact: fact
+      end
+    end
   end
 end
--- spec/factories/open_datasets.rb
+++ spec/factories/open_datasets.rb
@@ -3,5 +3,13 @@ FactoryGirl.define do
     name Faker::Lorem.sentence
     uid "#{Faker::Lorem.characters(4)}-#{Faker::Lorem.characters(4)}"
     url Faker::Internet.url
+
+    factory :dataset_with_facts do
+      ignore { facts_count 3 }
+
+      after :create do |dataset, evaluator|
+        create_list :chicago_fact, evaluator.facts_count, open_dataset: dataset
+      end
+    end
   end
 end
