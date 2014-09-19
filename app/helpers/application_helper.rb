c229c674dd1792e825bcc623f6e97a115aaac217
Chris Stump <chris@mighty-nice.com>
2014-09-18 20:55:08 -0500

    style the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,10 +14,10 @@ module ApplicationHelper
 
   def source_links_for(fact)
     sets = fact.datasets.map do |set|
-      link_to set.name.underscore.humanize, set.uri, target: '_blank'
+      link_to set.name.underscore.humanize, set.uri, target: '_blank', title: I18n.t('source_title')
     end
 
-    I18n.t('sources', source_links: sets.join(', ')).html_safe
+    sets.join(', ').html_safe
   end
 
 end
