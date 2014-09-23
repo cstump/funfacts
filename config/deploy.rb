189d5b9b9853f0c32484ddd8c19914930a7bc994
Chris Stump <chris@mighty-nice.com>
2014-09-22 20:50:15 -0500

    fix release paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'funfacts'
 set :repo_url, 'git@github.com:cstump/funfacts.git'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
-set :unicorn_config_path, File.expand_path('./server/unicorn.rb', __dir__)
-set :nginx_template, File.expand_path('./server/nginx.conf', __dir__)
+set :unicorn_config_path, File.join(release_path, 'config', 'server', 'unicorn.rb')
+set :nginx_template, File.join(release_path, 'config', 'server', 'nginx.conf')
 
 after 'deploy:publishing', 'deploy:restart'
