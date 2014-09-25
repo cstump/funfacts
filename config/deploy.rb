8e6d4789a75e92ab015f45376bf4fc5fe4d59fd5
Chris Stump <chris@mighty-nice.com>
2014-09-25 10:22:50 -0500

    generate binstubs on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,5 +6,6 @@ set :repo_url, 'git@github.com:cstump/funfacts.git'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 set :unicorn_config_path, File.join(release_path, 'config', 'unicorn.rb')
+set :bundle_flags, '--deployment --quiet --binstubs'
 
 after 'deploy:publishing', 'deploy:restart'
