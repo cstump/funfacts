563af9c259474201791f09cc5e9014a917a1418b
Chris Stump <chris@mighty-nice.com>
2015-03-05 12:44:04 -0600

    we no longer need to seed datasets -- they'll be fetched and loaded via rake tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
 Region.find_or_create_by! name: 'chicago'
-
-Dataset.find_or_create_by!(
-  name: 'LStationEntries',
-  uid: 't2rn-p8d7',
-  uri: 'https://data.cityofchicago.org/Transportation/CTA-Ridership-L-Station-Entries-Monthly-Day-Type-A/t2rn-p8d7'
-)
