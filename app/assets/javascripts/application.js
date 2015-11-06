7d828f47381286a7c9964c0c29b9636f0b44eea8
Chris Stump <chris@mighty-nice.com>
2015-11-05 23:57:49 -0600

    get rid of the jquery infinite scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,6 @@
 //= require react
 //= require react_ujs
 //= require jquery
-//= require jquery.infinitescroll
 //= require bootstrap
 //= require funfacts
 //= require_tree ./components
-//= require chicago
