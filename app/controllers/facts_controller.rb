e45e44c683f8bed6994511110e390880a31fec4a
Chris Stump <chris@mighty-nice.com>
2015-11-13 21:30:09 -0600

    render facts as JSON when asked to do so

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,10 +1,17 @@
 class FactsController < ApplicationController
+  include FactsHelper
 
   def index
     @facts = Region.find_by(name: region.name)
                    .facts
                    .page(facts_params[:page])
                    .per facts_per_page
+
+
+    respond_to do |format|
+      format.html
+      format.json { render json: serialized_facts }
+    end
   end
 
 
--- spec/controllers/facts_controller_spec.rb
+++ spec/controllers/facts_controller_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe FactsController, type: :controller do
   include_context 'region'
 
+  let(:facts_per_page) { Secrets.facts_per_page }
+  let!(:facts) { create_list :fact, facts_per_page }
+
   it 'defaults page size to a configurable value' do
-    create_list :fact, Secrets.facts_per_page
     get :index
     expect(assigns(:facts).size).to eq Secrets.facts_per_page
+  end
+
+  it 'responds with JSON when asked for JSON' do
+    get :index, format: :json
+    parsed = JSON.parse(response.body)['facts']
+    expect(parsed.map{|obj| obj['id']}).to match_array facts.map(&:id)
   end
 
   describe 'region setting' do
@@ -23,9 +31,9 @@ RSpec.describe FactsController, type: :controller do
 
   describe 'with specified page size' do
     let(:facts_per_page) { 5 }
-    
+    let!(:facts) { create_list :fact, (facts_per_page + 2)}
+
     before do
-      create_list :fact, (facts_per_page + 2)
       get :index, per: facts_per_page
     end
 
@@ -46,5 +54,4 @@ RSpec.describe FactsController, type: :controller do
       )
     end
   end
-
 end
