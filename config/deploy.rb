ea5699b11e4ffebf8c75a1bf38b0e817333046aa
Chris Stump <chris@mighty-nice.com>
2015-11-23 23:42:13 -0600

    link .env file on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.2.1'
 
 set :application, 'funfacts'
 set :repo_url, 'git@github.com:cstump/funfacts.git'
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{.env config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 set :unicorn_config_path, File.join(release_path, 'config', 'unicorn.rb')
 set :bundle_flags, '--deployment --quiet --binstubs'
