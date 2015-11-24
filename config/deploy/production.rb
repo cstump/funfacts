b7af4f2538ce04e9176ecb9c52de64dae2ea3789
Chris Stump <chris@mighty-nice.com>
2015-11-23 23:55:57 -0600

    force dotenv on deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,8 @@ set :stage, 'production'
 set :branch, 'master'
 set :rvm_ruby_version, 'ruby-2.1.2'
 
+# make sure environment variables below are loaded
+require 'dotenv'
+Dotenv.load!
+
 server ENV['DEPLOY_SERVER'], user: ENV['DEPLOY_USER'], roles: %w{web app db}, port: ENV['DEPLOY_PORT']
